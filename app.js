c7aaa9434632b28060bceb169c944209f05872fa
yanyanRYAN <rpecarma@gmail.com>
2020-05-18 23:25:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["Buy Food",
+  "Cook Food",
+  "Eat Food"
+];
+const workItems = [];
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.set('view engine', 'ejs'); //use ejs as view engine
+
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+
+  const day = date.getDate();
+
+
+  res.render('list', {
+    listTitle: day,
+    newListItems: items
+  });
+
+})
+
+app.post("/", function(req, res) {
+  const item = req.body.newItem;
+
+  console.log(req.body);
+  console.log(item);
+
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+})
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+})
+
+app.post("/work", function(req, res) {
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.get("/about", function(req, res){
+  res.render("about");
+})
+
+app.listen(3000, function() {
+  console.log("Server started port 3000");
+})
---
+++ date.js
@@ -0,0 +1,26 @@
+//created our first module!!!
+
+exports.getDate = function() {
+  const today = new Date();
+
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+
+  return day = today.toLocaleDateString("en-US", options);
+}
+
+exports.getDay = function () {
+  const today = new Date();
+
+  const options = {
+    weekday: 'long',
+
+  };
+
+  return day = today.toLocaleDateString("en-US", options);
+}
+
+console.log(module.exports);
